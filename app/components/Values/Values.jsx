a31cd2ef615efd136e37e8dc6b754b2844c177a6
Antonina Honcharenko <104636066+AntoninaHoncharenko@users.noreply.github.com>
2023-11-30 13:58:12 +0100

    add project base settings

---
+++ app/components/Values/Values.jsx
@@ -0,0 +1,3 @@
+export const Values = () => {
+  return <section>Values</section>;
+};
---
+++ app/layout.js
@@ -0,0 +1,45 @@
+import { Oswald, Fira_Sans } from "next/font/google";
+import localFont from "next/font/local";
+import "./globals.scss";
+
+const oswald = Oswald({
+  subsets: ["latin"],
+  variable: "--font-oswald",
+  weight: "400",
+});
+
+const fira = Fira_Sans({
+  subsets: ["latin"],
+  variable: "--font-fira",
+  weight: ["400", "500"],
+});
+
+const gothic = localFont({
+  src: "../public/fonts/Fontspring-DEMO-allroundgothic-thick.otf",
+  display: "swap",
+  variable: "--font-gothic",
+});
+
+const saygon = localFont({
+  src: "../public/fonts/CASaygonTextTrial-Regular.otf",
+  display: "swap",
+  variable: "--font-saygon",
+});
+
+export const metadata = {
+  title: "Ecosolution",
+  description:
+    "Development and implementation of renewable non-polluting energy sources, generating power generation using energy wind, sun, water, biomass",
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body
+        className={`${fira.variable} ${oswald.variable} ${gothic.variable} ${saygon.variable}`}
+      >
+        {children}
+      </body>
+    </html>
+  );
+}
